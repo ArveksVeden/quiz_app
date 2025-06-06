d6679f9d46cdaa3fa2a943c6b86623b16737eb33
Artem Vedeneev <arveksveden@yandex.ru>
2025-06-06 17:56:40 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/quiz_app_quiz_app/', // <-- твой github repo
+  base: '/quiz_app/', // исправь на правильное имя!
 });
